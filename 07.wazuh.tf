a7718b5312441cb42cc8a9236eeb92eaf9d8e3c7
reduan2660 <alve.reduan@gmail.com>
2023-11-04 00:05:09 +0600

    Wazuh

---
+++ 07.wazuh.tf
@@ -0,0 +1,25 @@
+resource "aws_instance" "wazuh" {
+  
+  ami           = "ami-00abdd4ea7dc7ec29" # Ubuntu wih docker
+  instance_type = "t3.xlarge"             # Free tier
+
+  vpc_security_group_ids = [
+    aws_security_group.allow-http-https.id,
+    aws_security_group.allow-ssh.id
+  ]
+
+  subnet_id = aws_subnet.sbnt[0].id
+  key_name = "aws-personal"
+
+  tags = {
+    Name = "wazuh-server"
+  }
+}
+
+output "wazuh_public_ip" {
+  value = aws_instance.wazuh.public_ip
+}
+
+output "wazuh_public_dns" {
+  value = aws_instance.staging_backend.public_dns
+}
