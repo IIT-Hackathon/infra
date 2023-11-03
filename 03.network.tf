e731b28ac06ce9c268059fa6f83d9079c924b1fd
reduan2660 <alve.reduan@gmail.com>
2023-11-03 18:40:20 +0600

    Subnet

--- 03.network.tf
+++ 03.network.tf
@@ -5,3 +5,17 @@ resource "aws_vpc" "vpc" {
         Name = "textwizard-vpc"
     }
 }
+
+resource "aws_subnet" "sbnt" {
+
+    count  = length(var.subnets_cidr)
+    vpc_id = aws_vpc.vpc.id
+    
+    cidr_block              = element(var.subnets_cidr , count.index) 
+    availability_zone       = element(var.availability_zones , count.index)
+    map_public_ip_on_launch = true
+
+    tags = {
+        Name = "textwizard-subnet-${count.index + 1}"
+    }
+}
